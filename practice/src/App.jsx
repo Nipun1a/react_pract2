f7920a79cae43a6632878b8f7beeb6fa49ddcec1
nipun1a <nipun1a1b@gmail.com>
2025-10-25 09:51:32 +0530

    all done

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './components/Card.jsx';
 import './index.css';
 import Navbar from './components/Navbar.jsx';
+// ✅ Import images from src
+import yug from './components/yug.jpg';
+import nipun from './components/nipun.jpg';
 
 const App = () => {
   // <> </> React Fragment — used to avoid unnecessary div wrappers
@@ -10,9 +13,9 @@ const App = () => {
       <div>
         <Navbar />
         <h1>Hello, Buddy!</h1>
-        <Card user = "yug"/>
-        <Card user = "luffy" />
-        <Card user = "nipun" />
+        <Card user = "yug" des = "This is my React practice project." image = {yug} />
+        <Card user = "luffy" des = "This is my first  project." image = "https://imgs.search.brave.com/e-6am5XDGPcK1Q4Yg6YlE71iaRbZ6VeVhFt6mKIMuRA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/d2hhdHMteW91ci1m/YXZvcml0ZS1sdWZm/eS1pbWFnZS12MC1q/b2gxMHgweHdpaWUx/LmpwZWc_YXV0bz13/ZWJwJnM9NDBjZGM1/Y2E1Zjg5YThkNDIy/MTIyOGNmNDRlZjll/MDkyMWNjYjkyYw" />
+        <Card user = "nipun" des = "This is my best project." image = {nipun} />
       </div>
     </>
   );
--- practice/src/components/Card.jsx
+++ practice/src/components/Card.jsx
@@ -5,9 +5,9 @@ function Card(props) {
   console.log(props);
   return (
     <div className="card">
-      <img className="card-img" src="https://imgs.search.brave.com/e-6am5XDGPcK1Q4Yg6YlE71iaRbZ6VeVhFt6mKIMuRA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/d2hhdHMteW91ci1m/YXZvcml0ZS1sdWZm/eS1pbWFnZS12MC1q/b2gxMHgweHdpaWUx/LmpwZWc_YXV0bz13/ZWJwJnM9NDBjZGM1/Y2E1Zjg5YThkNDIy/MTIyOGNmNDRlZjll/MDkyMWNjYjkyYw" alt="luffy" />
+      <img className="card-img" src={props.image} alt={props.user} />
       <h2>{props.user}</h2>
-      <p>This is my React practice project.</p>
+      <p>{props.des}</p>
       <button>view details</button>
     </div>
   );
